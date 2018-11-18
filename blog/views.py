d8d533a3c7c1767adf80df6eb9d6895d9868967d
cjltsod <github.tsod@tsod.idv.tw>
2018-11-18 17:00:34 +0800

    Create frontstage

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,8 @@ from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_create(request):
